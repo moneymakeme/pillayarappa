01b38770499b7b31613cd61bdc9e7bd09d949130
moneymakeme <130539471+moneymakeme@users.noreply.github.com>
2024-09-07 07:28:55 +0530

    Add files via upload

---
+++ pillarappa.js
@@ -0,0 +1,9 @@
+function showCustomMessage() {
+    const messageElement = document.getElementById('message');
+    messageElement.textContent = 'Wishing you joy, peace,and prosperity on Ganesh Chaturthi and always!';
+    
+    const imagesElement = document.getElementById('images');
+    imagesElement.style.display = 'block'; // Show images
+}
+
+
